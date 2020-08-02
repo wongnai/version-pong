26964134afc6035844f708f84f213d7daab2e32a
gunphasin <phasin@wongnai.com>
2020-08-02 20:39:37 +0700

    add tagPrefix option

--- src/update/index.ts
+++ src/update/index.ts
@@ -3,23 +3,29 @@ import standardVersion from 'standard-version'
 import { PublishLevel } from 'types'
 import bumpBetaVersion from './bumpBetaVersion'
 
-const updatePackageJson = async (publishLevel: PublishLevel) => {
+const updatePackageJson = async (
+  publishLevel: PublishLevel,
+  tagPrefix?: string,
+) => {
   try {
     const spinner = Spinner('Tagging Package Version...').start()
+    const prefix = `${tagPrefix}@`
     switch (publishLevel) {
       case PublishLevel.BETA:
-        await bumpBetaVersion(Spinner)
+        await bumpBetaVersion(Spinner, tagPrefix)
         break
       case PublishLevel.MINOR:
         await standardVersion({
           releaseAs: 'minor',
           silent: true,
+          tagPrefix: prefix,
         })
         break
       case PublishLevel.MAJOR:
         await standardVersion({
           releaseAs: 'major',
           silent: true,
+          tagPrefix: prefix,
         })
         break
     }
